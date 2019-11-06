d85a185a61c0e1c2e09cd01eaf9d76e966b90a58
centeroftheuniverse <hardcorenzzz@gmail.com>
2019-11-06 17:12:02 +0000

    Comment

---
+++ js/index.js
@@ -0,0 +1,42 @@
+//alert('Hello!');
+//let message = 'Hello!';
+//alert(message);
+//message = 'World!';
+//alert(message);
+//message = 'Hello World!';
+//alert(message);
+//const year = 2019;
+//alert('Now is ' +year);
+///const nextYear = year + 1;
+//alert('Next year will be ' + nextyear);
+
+//const userName = prompt('Enter your name');
+//alert('Hello ' + userName);
+const age = parseFloat(prompt('Enter your age'));
+const userAge = addOne(age)
+alert(userAge);
+//const userAge = age +1;
+//alert(userName +' next year you will be ' + userAge);
+if ( age < 18 ) {
+alert('go away');
+}else{
+alert('o k');
+}
+function addOne(num){
+    const result = num + 1;
+    return result;
+}
+
+// < > <= >= == === != !== || &&
+// All scalar data types in JS
+// String
+// Number
+// Boolean
+// undefined
+// null
+// Object
+// Symbol
+
+
+
+
